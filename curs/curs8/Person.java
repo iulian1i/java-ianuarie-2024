4fa56bdb9a34c5af659ff8c61e4c9346d17c7ccf
Iulian <35178081+geo600@users.noreply.github.com>
2024-02-20 20:31:31 +0200

    curs08

---
+++ curs/curs8/Person.java
@@ -0,0 +1,20 @@
+package curs.curs8;
+
+//o clasa este ca o schita
+public class Person {
+
+    /**
+     * proprietati
+     */
+    String name;
+    int age;
+
+    /**
+     * constructor
+     */
+    Person(String nameParam, int ageParam) {
+        this.name = nameParam;
+        this.age = ageParam;
+    }
+
+}
