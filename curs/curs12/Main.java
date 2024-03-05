6cf507302a3efc62ba734d40a448ea2060dbc890
Iulian <35178081+geo600@users.noreply.github.com>
2024-03-05 20:28:46 +0200

    curs12-updated

--- curs/curs12/Main.java
+++ curs/curs12/Main.java
@@ -1,5 +1,9 @@
 package curs.curs12;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -27,7 +31,25 @@ public class Main {
         double sumaContract = 100.22; //Double
         int sumaContractIntreg = 100; //Integer
 
-        System.out.println(sumaContract);
+        String sumaContractString = String.valueOf(sumaContract);
+
+        System.out.println(sumaContractString);
+
+        /**LocalDateTime */
+        LocalDate dataCurenta = LocalDate.now();
+        System.out.println(dataCurenta);
+
+        LocalDateTime momentulCurent = LocalDateTime.now();
+
+        LocalDate dataDeIeri = LocalDate.of(2024, 3, 4);
+        System.out.println(dataDeIeri);
+
+        System.out.println(LocalDate.now().minusDays(1));
+
+        LocalDateTime momentulDeIeri = LocalDateTime.of(2024, 3, 4, 10, 40, 50);
+        System.out.println(momentulDeIeri.format(DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm:ss")));
+
+//        System.out.println("    Leu     ".trim());
 
     }
 
